b894ef141d3f398a29c6ddbffac7c6ae0f204ab7
Jesus Guio <jfranciscoguio@protonmail.com>
2021-07-06 14:10:21 -0500

    Change slide button color according to the slide index

--- scripts/mine.js
+++ scripts/mine.js
@@ -1,7 +1,7 @@
-var slideIndex = 1;
+let slideIndex = 1;
 
-function changeDesign(array,color){
-    for(i = 0; i < array.length; i++){
+function changeDesign(array, color){
+    for(let i = 0; i < array.length; i++){
         array[i].style.color = color;
     }
 }
@@ -17,44 +17,44 @@ function currentSlide(n){
 }
 
 function showSlides(n){
-    var i;
-    var container = document.getElementsByClassName("container-general")[0];
-    var menu = document.getElementsByTagName("h3");
-    var title = container.getElementsByTagName("h2");
-    var text = container.getElementsByTagName("p");
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-   
-    if (n > slides.length){
-        slideIndex = 1
-    }
+    let i;
+    const container = document.getElementsByClassName("container-general")[0];
+    const menu = document.getElementsByTagName("h3");
+    const title = container.getElementsByTagName("h2");
+    const text = container.getElementsByTagName("p");
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+    const buttonSlide = document.getElementById("button-slide");
 
-    if (n % 2 == 0) {
-        container.style.background = "linear-gradient(to right, #599B96 0%,#599B96 50%, #42817E 50%,#42817E 100%)"; 
+    if (n > slides.length)
+        slideIndex = 1
+
+    if (n % 2 === 0) {
+        container.style.background = "linear-gradient(to right, #599B96 0%,#599B96 50%, #42817E 50%,#42817E 100%)";
         changeDesign(title,"#fff")
         changeDesign(text,"#fff")
-        changeDesign(menu,"#fff")    
+        changeDesign(menu,"#fff")
 
-        
+        buttonSlide.style.backgroundColor = "#68CBCC";
+        buttonSlide.style.boxShadow = "0 3px 5px 0 #68CBCC, 0 3px 5px 0 #68CBCC"
     }else{
         container.style.background = "linear-gradient(to right, #E7BC38 0%,#E7BC38 50%, #C68F11 50%,#C68F11 100%)";
         changeDesign(title, "#000")
         changeDesign(text,"#000")
         changeDesign(menu,"#000")
-    }
 
+        buttonSlide.style.backgroundColor = "#C68F11"
+        buttonSlide.style.boxShadow = "0 3px 5px 0 #C68F11, 0 3px 5px 0 #C68F11"
+    }
     
-    if (n < 1){
+    if (n < 1)
         slideIndex = slides.length
-    }
 
-    for(i = 0; i < slides.length; i++){
+    for (i = 0; i < slides.length; i++)
         slides[i].style.display = "none";
-    }
     
-    for(i = 0; i < dots.length; i++){
+    for(i = 0; i < dots.length; i++)
         dots[i].className = dots[i].className.replace(" active", " ");
-    }
 
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
