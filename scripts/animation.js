5e062d5e4497d2b3095bd792e69675779039b68f
Jesus Guio <jfranciscoguio@protonmail.com>
2021-07-27 14:02:43 -0500

    Changes in animation system

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,20 +1,18 @@
-let master = document.getElementById('building');
-console.log(master);
-master.addEventListener('scroll', function(){
-    console.log("test");
-    let top = this.scrollTop;
-    let bottom = top+this.offsetHeight;
-    
-    
-    this.querySelectorAll("div").forEach(function(div){
-        console.log(div);
-      if (
-        (div.offsetTop < top && top <div.offsetTop+div.offsetHeight) ||
-        (div.offsetTop < bottom && bottom <div.offsetTop+div.offsetHeight)
-      ){
-        div.classList.add('animation');
-      }
+document.addEventListener('scroll', function(){
+    let building = document.getElementById('building');
+
+    building.querySelectorAll("div").forEach(function(div){
+      let top = document.body.scrollTop;
+      let bottom = top + document.body.offsetHeight;
+
+      console.log("Top " + top)
+      console.log("Botton " + bottom)
+
+      if ((div.offsetTop < top && top < div.offsetTop + div.offsetHeight) ||
+          (div.offsetTop < bottom && bottom < div.offsetTop + div.offsetHeight)) {
+            div.classList.add('animation');
+        }
     });
-    
+
 });
-  
+ 
